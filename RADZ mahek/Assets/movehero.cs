b4bc79c139fd60de4627aa96acc1d4c76d6a328d
STUDENT Mahek Ahmed <Mahek.Ahmed@students.ittralee.ie>
2024-10-02 12:49:37 +0100

    Signed-off-by: STUDENT Mahek Ahmed <Mahek.Ahmed@students.ittralee.ie>

--- RADZ mahek/Assets/movehero.cs
+++ RADZ mahek/Assets/movehero.cs
@@ -4,28 +4,35 @@ using UnityEngine;
 
 public class movehero : MonoBehaviour
 {
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        if (animator == null)
+            print("Could not find Animator Component");
+        else
+            print("Animator Component found");
     }
 
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("isrunning", false);
         if (Input.GetKey(KeyCode.W))
+        {
             transform.position += transform.forward * Time.deltaTime;
+            animator.SetBool("isrunning", true);
+        }
 
         if (Input.GetKey(KeyCode.A))
-            transform.Rotate(Vector3.up, 30*Time.deltaTime);
+            transform.Rotate(Vector3.up, 30 * Time.deltaTime);
 
         if (Input.GetKey(KeyCode.D))
             transform.Rotate(Vector3.down, 30 * Time.deltaTime);
 
-       
-
-        
-
-
+        if (Input.GetKey(KeyCode.S))
+            transform.position += Vector3.back * Time.deltaTime;
+    
     }
 }
